7f88666d3ed3e8fd2bedc5c825ba3d7105655195
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-06 23:18:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
+def sort_array_asc(array)
+  return array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array = [array[0],array[2],array[1]]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.map {|e| e[2] = "$"; e}
+end
